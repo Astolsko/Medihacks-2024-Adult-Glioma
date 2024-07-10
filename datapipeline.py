48a588107f77dd0575df87f032fc82b8086a772a
Astolsko <82147523+Astolsko@users.noreply.github.com>
2024-07-11 04:23:10 +0530

    V.7
    Training Ended on UNET
    Model weights saved V-1.hdf5
    Results in CSV file

--- datapipeline.py
+++ datapipeline.py
@@ -7,36 +7,23 @@ from matplotlib import pyplot as plt
 import random
 
 def load_img(img_dir , img_list):
-    
     images = []
-    for i,image_name in enumerate(img_list):
-        
+    for i,image_name in enumerate(img_list):  
         if(image_name.split('.')[1]=='npy'):
-            # if the file is numpy array
-            
             image = np.load(img_dir + image_name)
-            images.append(image)
-        
-    images = np.array(images)
-        
+            images.append(image)    
+    images = np.array(images)    
     return (images)
 def imageLoader(img_dir , img_list , mask_dir , mask_list , batch_size):
-    
     L = len(img_list)
-    
-    while True:
-        
+    while True: 
         batch_start = 0
         batch_end = batch_size
-        
-        while batch_start < L:
-            
+        while batch_start < L: 
             limit = min(batch_end , L)
-            
-            X = load_img(img_dir , img_list[batch_start:limit]) # load image
-            Y = load_img(mask_dir , mask_list[batch_start:limit]) # load mask
-            
-            yield(X,Y) # yeilds images - a tuple with 2 numpy arrays with batch_size samples
+            X = load_img(img_dir , img_list[batch_start:limit]) 
+            Y = load_img(mask_dir , mask_list[batch_start:limit]) 
+            yield(X,Y) 
             
             batch_start += batch_size
             batch_end += batch_size
